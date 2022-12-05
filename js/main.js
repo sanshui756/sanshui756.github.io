dcec93b0664168228660df06389ba7af3d19e7a2
sanshui756 <252713321@qq.com>
2022-12-05 13:32:32 +0800

    Site updated: 2022-12-05 13:32:31

--- js/main.js
+++ js/main.js
@@ -776,3 +776,13 @@ document.addEventListener('DOMContentLoaded', function () {
   refreshFn()
   unRefreshFn()
 })
+if (document.querySelector('#bber-talk')) {
+  var swiper = new Swiper('.swiper-container', {
+    direction: 'vertical', // 垂直切换选项
+    loop: true,
+    autoplay: {
+      delay: 3000,
+      pauseOnMouseEnter: true
+    },
+  });
+}
